6173455604e0aacd8e927eda0e7e5a1039271036
cquxyw <bear_94@163.com>
2020-03-21 11:15:15 +0800

    simple

---
+++ scout/src/old/ppo_algo.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+model_path = '/home/xyw/BUAA/Graduation/src/scout/result/model/ppo.ckpt'
+dir_path = '/home/xyw/BUAA/Graduation/src/scout/result/model/'
+
+GAMMA = 0.9
+A_LR = 0.00001
+C_LR = 0.00002
+A_UPDATE_STEPS = 30
+C_UPDATE_STEPS = 30
+S_DIM, A_DIM = 2, 2
+METHOD = [
+    dict(name='kl_pen', kl_target=0.01, lam=0.5),   # KL penalty
+    dict(name='clip', epsilon=0.2),                 # Clipped surrogate objective, find this is better
+][1]        # choose the method for optimization
+
+class ppo(object):
+
+    def __init__(self):
+        self.sess = tf.Session()
+        self.tfs = tf.placeholder(tf.float32, [None, S_DIM], 'state')
+
+        # critic
+        with tf.variable_scope('critic'):
+            l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu)
+            self.v = tf.layers.dense(l1, 1)
+            self.tfdc_r = tf.placeholder(tf.float32, [None, 1], 'discounted_r')
+            self.advantage = self.tfdc_r - self.v
+            self.closs = tf.reduce_mean(tf.square(self.advantage))
+            self.ctrain_op = tf.train.AdamOptimizer(C_LR).minimize(self.closs)
+
+        # actor
+        pi, pi_params = self._build_anet('pi', trainable=True)
+        oldpi, oldpi_params = self._build_anet('oldpi', trainable=False)
+
+        with tf.variable_scope('sample_action'):
+            self.sample_op = tf.squeeze(pi.sample(1), axis=0)       # choosing action
+        with tf.variable_scope('update_oldpi'):
+            self.update_oldpi_op = [oldp.assign(p) for p, oldp in zip(pi_params, oldpi_params)]
+
+        self.tfa = tf.placeholder(tf.float32, [None, A_DIM], 'action')
+        self.tfadv = tf.placeholder(tf.float32, [None, 1], 'advantage')
+
+        with tf.variable_scope('loss'):
+            with tf.variable_scope('surrogate'):
+                ratio = pi.prob(self.tfa) / oldpi.prob(self.tfa)
+                surr = ratio * self.tfadv
+
+            self.aloss = -tf.reduce_mean(tf.minimum(
+                surr,
+                tf.clip_by_value(ratio, 1.-METHOD['epsilon'], 1.+METHOD['epsilon'])*self.tfadv))
+
+        with tf.variable_scope('atrain'):
+            self.atrain_op = tf.train.AdamOptimizer(A_LR).minimize(self.aloss)
+
+        tf.summary.FileWriter("/home/xyw/BUAA/Graduation/src/scout/result/log/", self.sess.graph)
+        self.sess.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()
+        if os.path.exists(model_path):
+            self.restorer = tf.train.import_meta_graph(model_path+'.meta')
+
+    def update(self, s, a, r):
+        self.sess.run(self.update_oldpi_op)
+        adv = self.sess.run(self.advantage, {self.tfs: s, self.tfdc_r: r})
+        
+        # update actor
+        [self.sess.run(self.atrain_op, {self.tfs: s, self.tfa: a, self.tfadv: adv}) for _ in range(A_UPDATE_STEPS)]
+
+        # update critic
+        [self.sess.run(self.ctrain_op, {self.tfs: s, self.tfdc_r: r}) for _ in range(C_UPDATE_STEPS)]
+
+    def _build_anet(self, name, trainable):
+        with tf.variable_scope(name):
+            l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu, trainable=trainable)
+            mu = 2 * tf.layers.dense(l1, A_DIM, tf.nn.tanh, trainable=trainable)
+            sigma = tf.layers.dense(l1, A_DIM, tf.nn.softplus, trainable=trainable)
+            norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)   
+        params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
+        return norm_dist, params
+
+    def choose_action(self, s):
+        s = s[np.newaxis, :]    
+        a = self.sess.run(self.sample_op, {self.tfs: s})[0]
+        if a[0] > 2:
+            a[0] = 2
+        elif a[0] < 0:
+            a[0] = 0
+        return np.clip(a, -2, 2)
+
+    def get_v(self, s):
+        if s.ndim < 2: s = s[np.newaxis, :]
+        return self.sess.run(self.v, {self.tfs: s})[0, 0]
+    
+    def save(self,times):
+        itimes = times
+        self.saver.save(self.sess, dir_path+'ppo_%i.ckpt' %(itimes))
+    
+    def restore(self):
+        if os.path.exists(model_path):
+            self.restorer.restore(self.sess,tf.train.latest_checkpoint(model_path))
