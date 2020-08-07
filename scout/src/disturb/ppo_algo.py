0b751bed720e6b76c53b0471fb15fd5088d3e8cc
cquxyw <bear_94@163.com>
2020-08-07 21:55:11 +0800

    school

---
+++ scout/src/disturb/ppo_algo.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+GAMMA = 0.9
+A_UPDATE_STEPS = 10
+C_UPDATE_STEPS = 10
+S_DIM, A_DIM = 5, 2
+METHOD = [
+    dict(name='kl_pen', kl_target=0.01, lam=0.5),   # KL penalty
+    dict(name='clip', epsilon=0.2),                 # Clipped surrogate objective, find this is better
+][1]        # choose the method for optimization
+
+class ppo(object):
+
+    def __init__(self):
+        self.sess = tf.compat.v1.Session
+        self.tfs = tf.compat.v1.placeholder(tf.float32, [None, S_DIM], 'state')
+        
+        self.A_LR = 1e-5
+        self.C_LR = 2e-5
+
+        # critic
+        with tf.compat.v1.variable_scope('critic'):
+            l1 = tf.keras.layers.Dense(self.tfs, 100, tf.nn.relu)
+            self.v = tf.keras.layers.Dense(l1, 1)
+            self.tfdc_r = tf.compat.v1.placeholder(tf.float32, [None, 1], 'discounted_r')
+            self.advantage = self.tfdc_r - self.v
+            self.closs = tf.reduce_mean(tf.square(self.advantage))
+            self.ctrain_op = tf.compat.v1.train.AdamOptimizer(self.C_LR).minimize(self.closs)
+
+        # actor
+        pi, pi_params = self._build_anet('pi', trainable=True)
+        oldpi, oldpi_params = self._build_anet('oldpi', trainable=False)
+
+        with tf.compat.v1.variable_scope('sample_action'):
+            self.sample_op = tf.squeeze(pi.sample(1), axis=0)       # choosing action
+        with tf.compat.v1.variable_scope('update_oldpi'):
+            self.update_oldpi_op = [oldp.assign(p) for p, oldp in zip(pi_params, oldpi_params)]
+
+        self.tfa = tf.compat.v1.placeholder(tf.float32, [None, A_DIM], 'action')
+        self.tfadv = tf.compat.v1.placeholder(tf.float32, [None, 1], 'advantage')
+
+        with tf.compat.v1.variable_scope('loss'):
+            with tf.compat.v1.variable_scope('surrogate'):
+                ratio = pi.prob(self.tfa) / oldpi.prob(self.tfa)
+                surr = ratio * self.tfadv
+
+            self.aloss = -tf.reduce_mean(tf.minimum(
+                surr,
+                tf.clip_by_value(ratio, 1.-METHOD['epsilon'], 1.+METHOD['epsilon'])*self.tfadv))
+
+        with tf.compat.v1.variable_scope('atrain'):
+            self.atrain_op = tf.compat.v1.train.AdamOptimizer(self.A_LR).minimize(self.aloss)
+
+        tf.compat.v1.summary.FileWriter("/home/xyw/BUAA/Graduation/src/scout/result/log/", self.sess.graph_def)
+        self.sess.run(tf.compat.v1.global_variables_initializer())
+
+    def update(self, s, a, r):
+        self.sess.run(self.update_oldpi_op)
+        adv = self.sess.run(self.advantage, {self.tfs: s, self.tfdc_r: r})
+        
+        # update actor
+        [self.sess.run(self.atrain_op, {self.tfs: s, self.tfa: a, self.tfadv: adv}) for _ in range(A_UPDATE_STEPS)]
+
+        # update critic
+        [self.sess.run(self.ctrain_op, {self.tfs: s, self.tfdc_r: r}) for _ in range(C_UPDATE_STEPS)]
+
+    def _build_anet(self, name, trainable):
+        with tf.compat.v1.variable_scope(name):
+            l1 = tf.keras.layers.Dense(self.tfs, 100, tf.nn.relu, trainable=trainable)
+            mu = 2 * tf.keras.layers.Dense(l1, A_DIM, tf.nn.tanh, trainable=trainable)
+            sigma = tf.keras.layers.Dense(l1, A_DIM, tf.nn.softplus, trainable=trainable)
+            norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)   
+        params = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES, scope=name)
+        return norm_dist, params
+
+    def choose_action(self, s):
+        s = s[np.newaxis, :]    
+        a = self.sess.run(self.sample_op, {self.tfs: s})[0]
+        return a
+
+    def get_v(self, s):
+        if s.ndim < 2: s = s[np.newaxis, :]
+        return self.sess.run(self.v, {self.tfs: s})[0, 0]
+    
+    def save(self, TRAIN_TIME):
+        dir_path = '/home/xyw/Train_Result/single/model/PPO_%i.ckpt' %(TRAIN_TIME)
+        saver = tf.compat.v1.train.Saver()
+        saver.save(self.sess, dir_path)
+    
+    def restore(self, TRAIN_TIME):
+        model_path = '/home/xyw/Train_Result/single/model/PPO_%i.ckpt' %(TRAIN_TIME)
+        if os.path.exists(model_path):
+            restorer = tf.compat.v1.train.import_meta_graph(model_path+'.meta')
+            restorer.restore(self.sess, tf.train.latest_checkpoint(model_path))
+        else:
+            print('No pre-trained model exist')
+
+    def resetgraph(self):
+        tf.compat.v1.reset_default_graph()
---
+++ scout/src/disturb/ppo_env.py
@@ -0,0 +1,138 @@
+import rospy
+from scout.msg import RL_input_msgs
+from geometry_msgs.msg import Twist
+from vlp_fir.msg import obs_info
+from gazebo_msgs.msg import ContactsState
+
+import tensorflow as tf
+import numpy as np
+import math
+import os
+import random
+
+import subprocess
+
+class env(object):
+
+    def __init__(self):
+        self.limit_v = 1.5
+        self.limit_w = 0.785
+
+        self.goal_x = 6
+        self.goal_y = 6
+
+        self.limit_circle = 12
+        self.reach_goal_circle = 0.8
+        self.limit_overspeed = 12
+            
+    def set_action(self, action):
+        # set publisher
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        pub_msg = Twist()
+        # print(action)
+        
+        # clip action
+        action[0] = np.clip(action[0], -self.limit_v, self.limit_v)
+        action[1] = np.clip(action[1], -self.limit_w, self.limit_w)
+
+
+        # publish action
+        if not np.isnan(action[0]) or np.isnan(action[1]):
+            pub_msg.linear.x = action[0]
+            pub_msg.angular.z = action[1]
+            pub.publish(pub_msg)
+        else:
+            print('Warning: Action is NAN')
+
+    def get_robot_info(self):
+        data = rospy.wait_for_message('RLin', RL_input_msgs)
+        current_state_info = np.array([data.me_x, data.me_y, data.me_yaw, data.me_v, data.me_w])
+        return current_state_info
+    
+    def get_obs_info(self):
+        data = rospy.wait_for_message('obj_', obs_info)
+        if data.num >= 3:
+            current_obs_info = np.array([
+                data.x[0], data.y[0], data.len[0], data.width[0],
+                data.x[1], data.y[1], data.len[1], data.width[1],
+                data.x[2], data.y[2], data.len[2], data.width[2],
+            ])
+        elif data.num == 2:
+            current_obs_info = np.array([
+                data.x[0], data.y[0], data.len[0], data.width[0],
+                data.x[1], data.y[1], data.len[1], data.width[1],
+                0, 0, 0, 0,
+            ])
+        elif data.num == 1:
+            current_obs_info = np.array([
+                data.x[0], data.y[0], data.len[0], data.width[0],
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+            ])
+        elif data.num == 0:
+            current_obs_info = np.array([
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+            ])
+        
+        return current_obs_info
+    
+    def get_collision_info(self):
+        data = rospy.wait_for_message('bumper', ContactsState)
+        if len(data.states):
+            collide = 1
+        else:
+            collide = 0
+        return collide
+
+    def compute_param(self):
+        current_state_info = self.get_robot_info()
+
+        vec_current_point = np.array([current_state_info[0], current_state_info[1]])
+        vec_des_point = np.array([self.goal_x, self.goal_y])
+        current_dis_from_des_point = np.linalg.norm(vec_des_point - vec_current_point)
+
+        over_v = abs(current_state_info[3]) - self.limit_v
+        over_w = abs(current_state_info[4]) - self.limit_w
+        overspeed = math.hypot(over_v, over_w)
+
+        return overspeed, current_dis_from_des_point
+    
+    def compute_state(self):
+        current_state_info = self.get_robot_info()
+        
+        disturb_x = random.uniform(-1,1)
+        disturb_y = random.uniform(-1,1)
+
+        current_state_info[0] += disturb_x
+        current_state_info[1] += disturb_y
+
+        current_obs_info = self.get_obs_info()
+
+        # compute state
+        state = np.concatenate([current_state_info, current_obs_info])
+        return state
+    
+    def compute_reward(self, collide, overspeed, current_dis_from_des_point):
+
+        reward = 0
+
+        if collide == 1:
+            reward += -1
+
+        if current_dis_from_des_point < self.reach_goal_circle:
+            reward += 2
+
+        if current_dis_from_des_point > self.limit_circle:
+            reward += -1
+            
+        return reward
+
+    def set_init_pose(self):
+        self.reset_env()
+        init_state = self.compute_state()
+        return init_state
+
+    def reset_env(self):
+        subprocess.Popen(['rosservice','call','/gazebo/reset_world'])
